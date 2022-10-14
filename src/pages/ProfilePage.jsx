2819b1b6de809337a986f6a41c948df1f0e4c05b
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-14 23:01:44 +0700

    add page profile and register

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { WithRouter } from "../utils/Navigation";
+
+const ProfilePage = () => {
+  return (
+    <div>ProfilePage</div>
+  )
+}
+
+export default WithRouter(ProfilePage);
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "../pages/Login";
+import Register from "../pages/Register"
 import HomePage from "../pages/HomePage";
+import ProfilePage from "../pages/ProfilePage"
 import NotFound from "../pages/NotFound";
 
 const Index = () => {
@@ -10,7 +12,9 @@ const Index = () => {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

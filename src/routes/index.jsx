b689fab051394abd36859d704c3a9204539c387a
dwifakhri <fakhri.chusaini01@gmail.com>
2022-10-17 21:30:11 +0700

    add pages detail and anonym

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,15 +6,20 @@ import Register from "pages/Register"
 import HomePage from "pages/HomePage";
 import ProfilePage from "pages/ProfilePage"
 import NotFound from "pages/NotFound";
+import ContentDetail from "pages/ContentDetail"
+import HomeAnonym from "pages/HomeAnonym"
+
 
 const Index = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/anonym" element={<HomeAnonym />} />
         <Route exact path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/detail" element={<ContentDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

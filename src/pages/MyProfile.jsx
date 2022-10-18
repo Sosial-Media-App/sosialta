ba5d242f229673090d3c06bdd921e57636b98571
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-18 22:03:58 +0700

    add page myprofile/editprofile

---
+++ src/pages/MyProfile.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { WithRouter } from 'utils/Navigation'
+
+const MyProfile = () => {
+  return (
+    <div>MyProfile</div>
+  )
+}
+
+export default WithRouter(MyProfile);
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,20 +5,21 @@ import Login from "pages/Login";
 import Register from "pages/Register"
 import HomePage from "pages/HomePage";
 import ProfilePage from "pages/ProfilePage"
+import MyProfile from "pages/MyProfile"
 import NotFound from "pages/NotFound";
 import ContentDetail from "pages/ContentDetail"
 import HomeAnonym from "pages/HomeAnonym"
-
 
 const Index = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/anonym" element={<HomeAnonym />} />
-        <Route exact path="/" element={<Login />} />
+        <Route exact path="/anonym" element={<HomeAnonym />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/myprofile" element={<MyProfile />} />
         <Route path="/detail" element={<ContentDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>

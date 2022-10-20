abd86f98549fee94ee0bccd2103c58d8b3a7862b
Rizki Haddi Prayoga <rizkihaddiprayoga@gmail.com>
2022-10-20 15:57:18 +0700

    update navbar anonym

--- src/components/NavbarAnonym.jsx
+++ src/components/NavbarAnonym.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { FiSearch } from "react-icons/fi";
+
 const NavbarAnonym = () => {
   return (
-    <div className="navbar bg-white px-5 md:px-14 flex justify-between mb-3 sticky top-0 z-10 shadow-sm">
+    <div className="navbar bg-white px-24 py-3 flex justify-between sticky top-0 z-10 shadow-sm">
       <div>
         <Link to="/">
           <a href="#link" className="font-semibold text-2xl">
@@ -12,21 +14,24 @@ const NavbarAnonym = () => {
           </a>
         </Link>
       </div>
-      <div className="form-control ">
+      <div className="flex mt-form-control">
+        <button className="py-3.5 pl-3 pr-2 border-y-2 border-l-2 border-none bg-main rounded-l-lg ">
+          <FiSearch className="text-xl text-[#999999]" />
+        </button>
         <input
           type="text"
-          placeholder="Search"
-          className="bg-main rounded-lg p-2 hidden md:block"
+          placeholder="Cari"
+          className="bg-main input rounded-l-none rounded-lg hidden md:block"
         />
       </div>
       <div className="flex-none font-semi-bold text-xs space-x-3">
         <Link to="/login">
-          <button className="bg-[#FEF3EB] w-20 rounded-md py-2 px-3 text-delete ">
+          <button className="bg-[#FEF3EB] rounded-md py-2.5 px-8 font-semibold text-sm text-delete ">
             Log In
           </button>
         </Link>
         <Link to="/register">
-          <button className="bg-primary w-20 rounded-md py-2 px-3 text-white ">
+          <button className="bg-primary rounded-md py-2.5 px-8 font-semibold text-sm text-white ">
             Sign Up
           </button>
         </Link>

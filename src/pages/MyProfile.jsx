260b61ef3dc2c3f2a664c7402938ebe4a9b5a7f7
rayhanrndy <rayhanrendy17@gmail.com>
2022-10-20 10:02:10 +0700

    update profile&myprofile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import { WithRouter } from 'utils/Navigation'
+import React from "react";
+import { WithRouter } from "utils/Navigation";
+import Layout from "components/Layout";
+import CardSosmed from "components/CardSosmed";
+import CardSuggestion from "components/CardSuggestion";
+import { Stack, Box } from "@mui/material";
+import Navbar from "components/Navbar";
+import Sampul from "assets/bg-sampul-profile.svg";
+import Profile from "assets/profile.png";
 
 const MyProfile = () => {
   return (
-    <div>MyProfile</div>
-  )
-}
+    <Layout>
+      <Navbar />
+      <div>
+        <img src={Sampul} alt="background" />
+        <div className="mx-24 grid grid-cols-7 ">
+          <img src={Profile} alt="profile" />
+          <div className="my-28">
+            <p className="font-semibold text-2xl">Jordyn Saris</p>
+            <p className="font-medium text-base">@jordynsaris</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-1 divide-y divide-gray mx-28">
+          <div />
+          <button className="underline decoration-primary">Post</button>
+        </div>
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="center"
+          className="px-0 md:px-20"
+        >
+          <Box flex={2}>
+            <CardSosmed></CardSosmed>
+            <CardSosmed></CardSosmed>
+          </Box>
+          <CardSuggestion></CardSuggestion>
+        </Stack>
+      </div>
+    </Layout>
+  );
+};
 
 export default WithRouter(MyProfile);
